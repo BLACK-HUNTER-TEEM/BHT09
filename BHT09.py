ae357bc2fb6164db5400bc356b75e4984510a540
BLACK-HUNTER-TEEM <105848361+BLACK-HUNTER-TEEM@users.noreply.github.com>
2022-05-31 22:17:50 +0600

    BHT09

--- BHT09.py
+++ BHT09.py
@@ -32,7 +32,7 @@ from multiprocessing.pool import ThreadPool
 from requests.exceptions import ConnectionError
 from mechanize import Browser
 reload(sys)
-sys.setdefaultencoding('utf8')
+sys.setdefaultencoding('utf11')
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
@@ -110,7 +110,7 @@ id = []
 print 'AUTHOR : BD. JAHIED'
 print 'FACEBOOK :ROOT.OFF.JAHIED'
 print 'GITHUB : BLACK-HUNTER-TEEM'
-CorrectUsername = 'MR-JAHIED'
+CorrectUsername = 'BD-JAHIED'
 CorrectPassword = 'MR-RAKIB'
 loop = 'true'
 while loop == 'true':
